ecc10b3dad8f73c21231512ce471d9697991d591
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:35:33 +0000

    test_lca_code_editing__code-editing-metrics-668819e8502524f7__id_13

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -106,42 +106,38 @@ def get_param_sub_dependant(
     param_name: str,
     depends: params.Depends,
     path: str,
-    security_scopes: Optional[List[str]] = None,
-) -> Dependant:
-    assert depends.dependency
-    dependant = get_sub_dependant(
-        depends=depends,
-        dependency=depends.dependency,
-        path=path,
-        name=param_name,
-        security_scopes=security_scopes,
-    )
-    for query_param in dependant.query_params:
-        query_param_field = depends.dependency.model_fields.get(query_param.name)
-        if query_param_field:
-            query_param.field_info.description = (
-                query_param_field.description or query_param_field.title or ""
-            )
-    return dependant
+    def get_parameterless_sub_dependant(*, depends: params.Depends, path: str) -> Dependant:
+        assert callable(depends.dependency), "A parameter-less dependency must have a callable dependency"
+        dependant = get_sub_dependant(depends=depends, dependency=depends.dependency, path=path)
+        for query_param in dependant.query_params:
+            query_param_field = depends.dependency.model_fields.get(query_param.name)
+            if query_param_field:
+                query_param.field_info.description = query_param_field.description or query_param_field.title or ""
+        return dependant
 
-
-def get_parameterless_sub_dependant(*, depends: params.Depends, path: str) -> Dependant:
-    assert callable(
-        depends.dependency
-    ), "A parameter-less dependency must have a callable dependency"
-    dependant = get_sub_dependant(
-        depends=depends, dependency=depends.dependency, path=path
-    )
-    for query_param in dependant.query_params:
-        query_param_field = depends.dependency.model_fields.get(query_param.name)
-        if query_param_field:
-            query_param.field_info.description = (
-                query_param_field.description or query_param_field.title or ""
-            )
-    return dependant
-
-
-def get_sub_dependant(
+    def get_sub_dependant(
+        *,
+        depends: params.Depends,
+        dependency: Any,
+        path: str,
+        param_name: str,
+        security_scopes: Optional[List[str]] = None,
+    ) -> Dependant:
+        assert depends.dependency
+        dependant = get_sub_dependant(
+            depends=depends,
+            dependency=depends.dependency,
+            path=path,
+            name=param_name,
+            security_scopes=security_scopes,
+        )
+        for query_param in dependant.query_params:
+            query_param_field = depends.dependency.model_fields.get(query_param.name)
+            if query_param_field:
+                query_param.field_info.description = (
+                    query_param_field.description or query_param_field.title or ""
+                )
+        return dependant
     *,
     depends: params.Depends,
     dependency: Callable[..., Any],
